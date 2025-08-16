300dd996192d53773596ace14823baa9f5e910fc
nikobatzi1990 <nicole.boci@gmail.com>
2025-08-15 23:45:33 -0400

    chore: install react-select

---
+++ client/src/components/Dropdown.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Select from "react-select";
+
+const options = [
+  // { value: 'chocolate', label: 'Chocolate' },
+  
+];
+
+export default function Dropdown() {
+  return <Select options={options} />;
+}
